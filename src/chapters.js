6be22fa80a99fada96575671b5831614daabfe36
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-02-10 11:27:27 +0000

    Fixed logic
    Co-authored-by: Miah <78933903+miahbates@users.noreply.github.com>

--- src/chapters.js
+++ src/chapters.js
@@ -1,6 +1,6 @@
 const chapters = [
   {
-    id: 1,
+    id: 0,
     narration: "You leave home and a dog approaches you..",
     options: [
       {
@@ -16,55 +16,71 @@ const chapters = [
     ],
   },
   {
-    id: 2,
+    id: 1,
     narration:
       "You get to the tube station and you realise that you forgot your wallet .. what do you do?",
     options: [
       {
-        answerID: "a",
+        answerID: "c",
         answer: "Steal someone else's wallet 😜",
         correct: false,
       },
       {
-        answerID: "b",
+        answerID: "d",
         answer: "Decide to walk to FAC instead!",
         correct: true,
       },
     ],
   },
   {
-    id: 3,
+    id: 2,
     narration:
       "You meet Adam, and he offers you ride on motorbike, do you ride the bike or walk?",
     options: [
       {
-        answerID: "a",
+        answerID: "f",
         answer: "Ride the bike 😜",
         correct: false,
       },
       {
-        answerID: "b",
+        answerID: "g",
         answer: "Walk, you would never trust Adam riding a bike!",
         correct: true,
       },
       {
-        answerID: "b",
+        answerID: "h",
         answer: "Steal Adam's bike.",
         correct: false,
       },
     ],
   },
   {
-    id: 4,
+    id: 3,
     narration: "step 4",
     options: [
       {
-        answerID: "a",
+        answerID: "i",
         answer: "The dog looks really friendly, I stop and pet it!",
         correct: true,
       },
       {
-        answerID: "b",
+        answerID: "j",
+        answer: "The dogs tinks, I ignore it because I am cool like that",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    narration: "step 5",
+    options: [
+      {
+        answerID: "k",
+        answer: "The dog looks really friendly, I stop and pet it!",
+        correct: true,
+      },
+      {
+        answerID: "l",
         answer: "The dogs tinks, I ignore it because I am cool like that",
         correct: false,
       },
@@ -72,15 +88,15 @@ const chapters = [
   },
   {
     id: 5,
-    narration: "step 5",
+    narration: "step 6",
     options: [
       {
-        answerID: "a",
+        answerID: "m",
         answer: "The dog looks really friendly, I stop and pet it!",
         correct: true,
       },
       {
-        answerID: "b",
+        answerID: "n",
         answer: "The dogs tinks, I ignore it because I am cool like that",
         correct: false,
       },
@@ -88,15 +104,15 @@ const chapters = [
   },
   {
     id: 6,
-    narration: "step 6",
+    narration: "step 7",
     options: [
       {
-        answerID: "a",
+        answerID: "o",
         answer: "The dog looks really friendly, I stop and pet it!",
         correct: true,
       },
       {
-        answerID: "b",
+        answerID: "p",
         answer: "The dogs tinks, I ignore it because I am cool like that",
         correct: false,
       },
@@ -104,29 +120,25 @@ const chapters = [
   },
   {
     id: 7,
-    narration: "step 7",
+    narration: "You made it! Congrats!",
     options: [
       {
-        answerID: "a",
-        answer: "The dog looks really friendly, I stop and pet it!",
+        answerID: "q",
+        answer: "Play again!",
         correct: true,
-      },
-      {
-        answerID: "b",
-        answer: "The dogs tinks, I ignore it because I am cool like that",
-        correct: false,
       },
     ],
   },
   {
     id: 8,
-    narration: "You made it! Congrats!",
-    options: [],
-  },
-  {
-    id: 20,
     narration: "You LOST",
-    options: [],
+    options: [
+      {
+        answerID: "z",
+        answer: "Play again!",
+        correct: false,
+      },
+    ],
   },
 ];
 
